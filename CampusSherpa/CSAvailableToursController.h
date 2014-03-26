9dd9dcb5e1c25e5336f90aa44783f012a84beab1
Harihar Subramanyam <hsubrama@mit.edu>
2014-03-26 11:41:10 -0400

    Working on table view

---
+++ CampusSherpa/CSAvailableToursController.h
@@ -0,0 +1,13 @@
+//
+//  CSAvailableToursController.h
+//  CampusSherpa
+//
+//  Created by Harihar Subramanyam on 3/26/14.
+//  Copyright (c) 2014 Campus Sherpa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAvailableToursController : UITableViewController
+
+@end
